1782cb0d7acd0ec47770d1f743f9ddd22bfaca63
Nnorb <norbertnagy87@gmail.com>
2024-04-12 11:40:29 +0200

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import SearchBar from "./SearchBar.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddItem from "./AddItem";
 import ItemsDisplay from "./ItemsDisplay";
 
@@ -11,24 +11,56 @@ function App() {
   const updateFilters = (searchParams) => {
     setFilters(searchParams);
   };
-
+  
   const addItemToData = (item) => {
     let items = data["items"];
+
+
     item.id = items.length;
     items.push(item);
     setData({ items: items });
     console.log(data);
-  };
+
+  }; 
+
+  const filterData = (data) => {
+    const filteredData = [];
+
+    if (filters.name === undefined) {
+      return data;
+    }
+
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue;
+      }
+      
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue;
+      }
+
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue;
+      }
+    
+      filteredData.push(item);
+    }
+    
+    return filteredData;
+  }
 
   return (
     <div className="container">
       <div className="row mt-3">
-        <ItemsDisplay items={data["items"]} />
+        <ItemsDisplay items= {filterData(data["items"])} />
       </div>
       <div className="row mt-3">
         <SearchBar updateSearchParams={updateFilters} />
       </div>
-
       <div className="row mt-3">
         <AddItem addItem={addItemToData} />
       </div>
